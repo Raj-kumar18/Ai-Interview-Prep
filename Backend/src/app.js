1098df5e0b9977fb68db456dad77d5d8e017ca30
Raj-kumar18 <drajkumar01234@gmail.com>
2026-07-15 14:03:28 +0530

    feat: initialize backend express app and configure frontend SPA routing for Vercel deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev"))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-interview-prep-sigma-ruby.vercel.app",
     credentials: true
 }));
 
